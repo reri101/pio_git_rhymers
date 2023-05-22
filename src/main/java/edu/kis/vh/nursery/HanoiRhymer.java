1dbad6219c53a5d6012d946f10043018b0bfe150
240847@edu.p.lodz.pl <2404847@edu.p.lodz.pl>
2023-05-22 10:41:15 +0200

    17. Wygenerowanie i uzupenienie szkieletu  dokumentacji dla  HanoiRhymer

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -1,9 +1,18 @@
 package edu.kis.vh.nursery;
 
+/**
+ * Klasa HanoiRhymer dziedziczy po DefaultCountingOutRhymer i przesyla metode countIn
+ */
 public class HanoiRhymer extends DefaultCountingOutRhymer {
 
-    private int totalRejected = 0;
+    /**
+     * zmienna totalRejected przechowuje liczbe odrzuconych numerow
+     */     private int totalRejected = 0;
 
+
+    /**
+     * metoda reportRejected zwraca totalRejected
+     */
     public int reportRejected() {
         return totalRejected;
     }
